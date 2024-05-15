9f96fe922f91b30eb837312f3108797255956d23
Lorenzo Vegetti <lvegetti@biomas.it>
2024-05-15 11:44:04 +0200

    Rimozione di file che non devono stare in repo Aggiunto .gitignore

--- obj/Debug/net7.0/BudgetAnalyzer.AssemblyInfo.cs
+++ obj/Debug/net7.0/BudgetAnalyzer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetAnalyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713d134cd66378f0458ce03714de43b18adf3717")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetAnalyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetAnalyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
